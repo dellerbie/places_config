2e05b1d4e1062b5b8ee52a13be3318cbd8231a00
Derrick Ellerbie <dellerbie@gmail.com>
2012-03-01 16:58:14 -0800

    Use production mongo server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'capistrano/ext/multistage'
 
-set :user, "rails"
+set :stages, %w(staging production)
+set :default_stage, "staging"
 set :application, "places_config"
 set :domain, "grub.dellerbie.com"
-set :deploy_to, "/home/#{user}/#{application}"
 
 default_run_options[:pty] = true  # Must be set for the password prompt from git to work
 set :repository, "git@github.com:dellerbie/places_config.git"
@@ -12,10 +12,6 @@ set :scm_verbose, true
 set :use_sudo, false
 set :branch, "master"
 set :deploy_via, :remote_cache
-
-role :web, "dellerbie.com"
-role :app, "dellerbie.com"
-role :db,  "dellerbie.com", :primary => true
 
 namespace :deploy do
   task :start do ; end
